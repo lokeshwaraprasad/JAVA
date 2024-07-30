f58621ab49703e15d82e36c152288bb2781ce7a9
lokeshwaraprasad <141038939+lokeshwaraprasad@users.noreply.github.com>
2024-07-30 21:00:10 +0530

    Getting Input

---
+++ input.java
@@ -0,0 +1,16 @@
+import java .util.*;
+
+public class input {
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        String name=sc.nextLine();
+        int age=sc.nextInt();
+        System.out.println(name + "\n" + age);
+
+
+
+
+    }
+    
+}
