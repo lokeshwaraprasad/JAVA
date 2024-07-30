46f9d13342af544078c8845ae245b2d9b1baaefe
lokeshwaraprasad <141038939+lokeshwaraprasad@users.noreply.github.com>
2024-07-30 20:43:07 +0530

    Output

---
+++ output.java
@@ -0,0 +1,7 @@
+public class  output {
+    public static void main(String args[])
+    {
+         System.out.println("Variables in java");
+         System.out.println("lokeshwaraprasad\n welcome to JAVA");
+    }
+}
